caec74569e01479baa5a1add4892e42e84b81a89
Beksultan Kadyrov <scarpion.kg@gmail.com>
2019-07-18 19:14:34 +0600

    create Transaction models

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -20,3 +20,29 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+    def update_balance(self, balance, reason, user):
+        with transaction.atomic():
+            if reason is Transaction.REPLENISH:
+                User.objects.select_for_update().filter(id=user.id).filter(id=user.id).update(balance=user.balance+balance)
+            elif reason is Transaction.WITHDRAWAL:
+                User.objects.select_for_update().filter(id=user.id).update(balance=user.balance-balance)
+
+
+class Transaction(models.Model):
+    REPLENISH = 1
+    WITHDRAWAL = 2
+
+    CHOICES = (
+        (REPLENISH, 'Пополнить'),
+        (WITHDRAWAL, 'Вывод'),
+    )
+
+    user = models.ForeignKey(
+        'User', related_name='balance_changes',
+        on_delete=models.CASCADE
+    )
+
+    action = models.PositiveSmallIntegerField(choices=CHOICES, default=REPLENISH)
+    amount = models.DecimalField('Amount', default=0, max_digits=18, decimal_places=6)
+    created_time = models.DateTimeField(auto_now_add=True)
